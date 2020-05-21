6d02257b569e00c1d194fe142118610f2e2515e5
Frank Bennett <biercenator@gmail.com>
2020-05-21 11:22:38 +0900

    Add test-build script to repo

---
+++ testbuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# build plugin code
+node ./scripts/build.js
+
+# package plugin code
+cd ../zotero-build
+./xpi/build_xpi --source-dir=../jurism/build --channel=source --commit-hash=HEAD
+
+# build standalone
+cd ../zotero-standalone-build
+./build.sh -p l -d /media/storage/src/JM/zotero-build/xpi/build/staging -s
+
+# return code
+cd ../jurism
+
+# run program
+../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -d 1
+echo ../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -d 1
+
+#../zotero-standalone-build/staging/Zotero_linux-x86_64/zotero -jsconsole -d 1
+#echo ../zotero-standalone-build/staging/Zotero_linux-x86_64/zotero --ProfileManager -jsconsole -d 1
