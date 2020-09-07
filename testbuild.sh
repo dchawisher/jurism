32806c192551d318c9b3408d52652a2eaed712aa
Frank Bennett <biercenator@gmail.com>
2020-09-07 17:48:51 +0900

    Update testbuild.sh

--- testbuild.sh
+++ testbuild.sh
@@ -17,8 +17,9 @@ cd ../zotero-standalone-build
 cd ../jurism
 
 # run program
-../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -d 1
-echo ../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -d 1
+../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -ZoteroDebug
+#../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager
+echo ../zotero-standalone-build/staging/Jurism_linux-x86_64/jurism --ProfileManager -jsconsole -ZoteroDebug
 
 #../zotero-standalone-build/staging/Zotero_linux-x86_64/zotero -jsconsole -d 1
 #echo ../zotero-standalone-build/staging/Zotero_linux-x86_64/zotero --ProfileManager -jsconsole -d 1
